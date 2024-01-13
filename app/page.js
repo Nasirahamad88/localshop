e07655d3211f9ff2f0c696b55cc177a0e5ed6e7d
Nasirahamad88 <onlineworkbd88888@gmail.com>
2024-01-13 17:57:16 +0600

    auth

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      Hello
+      This is our Local Shop to help Local shop to sell there products
     </>
   )
 }
